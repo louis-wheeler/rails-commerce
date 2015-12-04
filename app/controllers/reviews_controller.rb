eb4916f3be935f3a29263ef8ea3e59c8e609b0b6
Louis Wheeler <louis.wheeler@trifecta.com>
2015-12-04 16:19:03 -0500

    Improved UI in various places

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,8 @@ class ReviewsController < ApplicationController
     def create
         @product = Product.find(params[:product_id])
         @review = @product.reviews.create(review_params)
+        @review.user = current_user.username
+        @review.update(review_params)
         redirect_to product_path(@product)
     end
     
